7a8f5bdfbef733580f25d0ea34e5297db3e0462d
Kamila Moura <78503528+KamilaMoura1@users.noreply.github.com>
2025-01-21 22:44:58 +0000

    Update database.js

--- database.js
+++ database.js
@@ -1,11 +1,29 @@
 import mysql from "mysql2/promise";
 
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "admin",
-  database: "walks",
+  host: "sql.freedb.tech",
+  user: "freedb_site-user",
+  password: "T!hzcUa3ngm2y!Q",
+  database: "freedb_db_site",
 });
+
+export const queryDatabase = async (q, vals = []) => {
+  const connection = await pool.getConnection();
+  try {
+    const [rows, fields] = await connection.query(q, vals);
+    return {
+      success: 1,
+      data: rows,
+    };
+  } catch (err) {
+    return {
+      success: 0,
+      error: err,
+    };
+  } finally {
+    connection.release();
+  }
+};
 
 export const queryDatabase = async (q, vals = []) => {
   const connection = await pool.getConnection();
