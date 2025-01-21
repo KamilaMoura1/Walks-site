625486a3d0d12111117a7df30fa330a0b827c9f7
Kamila Moura <78503528+KamilaMoura1@users.noreply.github.com>
2025-01-21 20:06:08 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const session = require("express-session");
-const passport = require("passport");
-const authRouter = require("./routes/auth_route.js");
-const { ensureAuthenticated } = require("./routes/auth_route.js");
-const { admin_router, getWalk } = require("./routes/admin.js");
-
+import express from "express";
+import bodyParser from "body-parser";
+import session from "express-session";
+import passport from "passport";
+import authRouter, { ensureAuthenticated } from "./routes/auth_route.js";
+import { admin_router, getWalk } from "./routes/admin.js";
 const app = express();
 
 app.use(express.static("public"));
@@ -51,5 +49,4 @@ app.get("/crud", ensureAuthenticated, async (req, res) => {
 //app.listen(3000, () => {
 //  console.log("Server running on http://localhost:3000");
 //});
-
-module.exports = app;
+export default app;
